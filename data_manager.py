fc4695de23a8ef9264df82f08822e91bb97a0c64
Andreea Grosu <andreea.grosu87@gmail.com>
2020-01-20 10:29:55 +0200

    Add first 4 tasks

--- data_manager.py
+++ data_manager.py
@@ -10,3 +10,68 @@ def get_mentor_names_by_first_name(cursor, first_name):
                    {'first_name': first_name})
     names = cursor.fetchall()
     return names
+
+
+@database_common.connection_handler
+def get_all_mentors_names(cursor):
+    cursor.execute("""
+                    SELECT first_name, last_name FROM mentors
+                    ORDER BY first_name;
+                   """,
+                   )
+    names = cursor.fetchall()
+    return names
+
+@database_common.connection_handler
+def get_mentors_nicknames(cursor, city):
+    cursor.execute("""
+                    SELECT nick_name FROM mentors
+                    WHERE city = %(city)s ORDER BY nick_name;
+                   """,
+                   {'city': city})
+    nicknames = cursor.fetchall()
+    return nicknames
+
+@database_common.connection_handler
+def get_full_name(cursor):
+    cursor.execute("""
+                    SELECT CONCAT(first_name, last_name) AS full_name, phone_number
+                    FROM applicants
+                    WHERE first_name = 'Carol' ;
+                   """)
+    full_name = cursor.fetchall()
+    return full_name
+
+@database_common.connection_handler
+def get_full_name_with_email(cursor):
+    cursor.execute("""
+                    SELECT CONCAT(first_name, last_name) AS full_name, phone_number
+                    FROM applicants
+                    WHERE email LIKE '%@adipiscingenimmi.edu' ;
+                   """)
+    full_name = cursor.fetchall()
+    return full_name
+
+@database_common.connection_handler
+def add_new_applicant(cursor, first_name, last_name, phone_number, email, application_code):
+    cursor.executemany("""
+                    INSERT INTO applicants (id, first_name, last_name, phone_number, email, application_code)
+                    VALUES (id.nextval, %(first_name)s, %(last_name)s, %(phone_number)s, %(email)s, %(application_code)s ;
+                   """,
+                   {'first_name': first_name},
+                   {'last_name': last_name},
+                   {'phone_number': phone_number},
+                   {'email': email},
+                   {'application_code': application_code})
+
+
+@database_common.connection_handler
+def display_new_applicant(first_name):
+    cursor.execute("""
+                    SELECT * FROM applicants
+                    WHERE first_name=%(first_name)s
+                    """,
+                   {'first_name': first_name}
+                   )
+    new_applicant=cursor.fetchall()
+    return new_applicant
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import data_manager
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,13 +9,57 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/mentors-with-best-first-name')
+@app.route('/all-mentors-by-name')
 def mentor_names():
-    # We get back dictionaries here (for details check 'database_common.py')
-    mentor_names = data_manager.get_mentor_names_by_first_name('László')
-
+    mentor_names = data_manager.get_all_mentors_names()
     return render_template('mentor_names.html', mentor_names=mentor_names)
+
+
+@app.route('/mentors-from-Miskolc-by-nickname')
+def mentor_nicknames():
+    mentor_names = data_manager.get_mentors_nicknames('Miskolc')
+    return render_template('nicknames.html', mentor_names=mentor_names)
+
+
+@app.route('/Carol-phonenumber')
+def carol_fullname():
+    applicant_names = data_manager.get_full_name()
+    return render_template('carol_hat.html', applicant_names=applicant_names)
+
+
+@app.route('/hat-owner-by-email')
+def name_by_email():
+    applicant_names = data_manager.get_full_name_with_email()
+    return render_template('name_by_email.html', applicant_names=applicant_names)
+
+
+@app.route('/add-new-applicant', methods=['GET', 'POST'])
+def add_new_applicant():
+    if request.method == 'GET':
+        return render_template('new_applicant.html')
+    elif request.method == 'POST':
+        return render_template('index.html')
+        # first_name = request.form['first_name']
+        # last_name = request.form['last_name']
+        # phone_number = request.form['phone_number']
+        # email = request.form['email']
+        # application_code = 54823
+        # return redirect('new_applicant_details.html',
+        #                        first_name=first_name,
+        #                        last_name=last_name,
+        #                        phone_number=phone_number,
+        #                        email=email,
+        #                        application_code=application_code
+        #                        )
+        # try:
+        #     add_new_applicant(cursor, first_name, last_name, phone_number, email, application_code)
+        #     new_name =  display_new_applicant(first_name)
+        #     return redirect('new_applicant_details.html', new_name=new_name)
+        # except:
+        #     return "issue???"
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
